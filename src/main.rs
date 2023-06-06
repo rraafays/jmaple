4384fd646f6695561eee99be3d649c17e60576f5
raf <rraf@tuta.io>
2023-06-06 08:09:51 +0100

    update: learning how to use cursive

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use cursive::{
+    views::{Dialog, TextView},
+    Cursive,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    siv.add_layer(Dialog::text("...").title("...").button("next", show_next));
+
+    siv.run();
+}
+
+fn show_next(s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text("Do the thing")
+            .title("bruh moment")
+            .button("ok", |s| ())
+            .button("not okay", |s| ()),
+    );
 }
